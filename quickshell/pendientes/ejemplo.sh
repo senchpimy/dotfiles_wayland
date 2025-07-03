3e3718312f253824f275e5226b4cb7e954c42e17
senchpimy <senchpimy@gmail.com>
2025-07-02 18:01:22 -0600

    ejemplo

---
+++ quickshell/pendientes/ejemplo.sh
@@ -0,0 +1,4 @@
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"path": "~/Documents/PythonProjects/Eiri/secretariobot/pro/horarios/2025-07-01.md"}' \
+  http://localhost:8080/set-path
