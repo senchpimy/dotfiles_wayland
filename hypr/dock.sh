6fe10843408ac27c677ad0df386dc2ff6e9383f7
senchpimy <senchpimy@gmail.com>
2025-07-14 22:26:38 -0600

    hyprshake mouse deprecaded

--- hypr/dock.sh
+++ hypr/dock.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sleep 5
-# Start the dock
-INITIAL_LAUNCH_FLAGS=" -r -i 64 -w 10 -mb 6 -hd 0 -c 'qs -p /home/plof/.config/quickshell/app_launcher/main.qml' -ico '/usr/share/icons/kora/actions/symbolic/view-app-grid-symbolic.svg'"
-hyprctl dispatch exec "nwg-dock-hyprland $INITIAL_LAUNCH_FLAGS"
-sleep 1
-hyprshakemouse
+#sleep 5
+## Start the dock
+#INITIAL_LAUNCH_FLAGS=" -r -i 64 -w 10 -mb 6 -hd 0 -c 'qs -p /home/plof/.config/quickshell/app_launcher/main.qml' -ico '/usr/share/icons/kora/actions/symbolic/view-app-grid-symbolic.svg'"
+#hyprctl dispatch exec "nwg-dock-hyprland $INITIAL_LAUNCH_FLAGS"
+#sleep 1
+#hyprshakemouse
