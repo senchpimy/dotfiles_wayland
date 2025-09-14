0459168d17c035adf9b3fe5d9292968a1ad5b442
senchpimy <senchpimy@gmail.com>
2025-09-14 07:59:24 -0600

    packages needed

---
+++ needed.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+sudo pacman -S git curl wget
+
+if ! command -v paru &> /dev/null
+then
+    sudo pacman -S --needed base-devel
+    git clone https://aur.archlinux.org/paru.git
+    cd paru
+    makepkg -si
+    cd ..
+    rm -rf paru
+fi
+
+paru -S --needed \
+    alacritty \
+    alsa \
+    bat \
+    base-devel \
+    blender \
+    bottom \
+    chafa \
+    cowsay \
+    cpio \
+    eww \
+    fprintd \
+    fastfetch \
+    flameshot \
+    fortune-mod \
+    gimp \
+    gnome-control-center \
+    gradience \
+    grim \
+    htop \
+    hypridle \
+    hyprpolkitagent \
+    jq \
+    kdenlive \
+    keepassxc \
+    kora-icon-theme \
+    lazygit \
+    lf \
+    luarocks \
+    ly \
+    matugen \
+    neo-matrix \
+    neofetch \
+    neovim \
+    neovide \
+    noto-fonts-cjk \
+    npm \
+    nwg-look \
+    obsidian \
+    pamixer \
+    parallel \
+    pfetch \
+    pipewire-alsa \
+    platformio \
+    playerctl \
+    pokemon-colorscripts \
+    pywal \
+    python-pip \
+    qt5-base \
+    qt5-graphicaleffects \
+    qt5-svg \
+    qt5-tools \
+    qt5-wayland \
+    qt6-5compat \
+    qt6-graphicaleffects \
+    ripgrep \
+    rofi-wayland \
+    sccache \
+    screen \
+    screenfetch \
+    spotify \
+    starship \
+    swww \
+    texlive-core \
+    texlive-latexextra \
+    texlive-pictures \
+    texlive-science \
+    tlp \
+    tmux \
+    ttf-cascadia-code \
+    vim \
+    wget \
+    wlogout \
+    yazi \
+    zathura \
+    zathura-pdf-mupdf \
+    zen-browser \
+    zsh 
