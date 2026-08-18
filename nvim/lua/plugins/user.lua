ade54dcb7cdb3610899c78a70249e3d039450b00
senchpimy <senchpimy@gmail.com>
2026-08-18 09:07:18 -0600

    nvim updates

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -384,7 +384,7 @@ return {
   },
   --{ 'andymass/vim-matchup' },
   {
-    "phaazon/hop.nvim",
+    "yuki-yano/hop.nvim",
     config = function() require("hop").setup() end,
   },
   { "lambdalisue/suda.vim" },
